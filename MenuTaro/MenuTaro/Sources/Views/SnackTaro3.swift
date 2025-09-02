ab66744f40320eacb3c2857550b6641be3f579c6
zzaeun <leeje57575@gmail.com>
2025-09-02 15:09:49 +0900

    [Feat] #9 - 쿠키가 깨지는 동안 문구가 변하는 기능 구현

--- MenuTaro/MenuTaro/Sources/Views/SnackTaro3.swift
+++ MenuTaro/MenuTaro/Sources/Views/SnackTaro3.swift
@@ -8,17 +8,25 @@
 import SwiftUI
 
 struct SnackTaro3: View {
+    @State private var text = "과연..!"
     var body: some View {
         VStack {
-            Text("과연..!")
+            Text(text)
                 .multilineTextAlignment(.center)
-                    .font(.system(size: 28, weight: .semibold))
-                    .padding(.top, 176)
-                    .padding(.bottom, 108)
+                .font(.system(size: 28, weight: .semibold))
+                .padding(.top, 176)
+                .padding(.bottom, 108)
             cookieCircle(imageName: "cookie_broken_snack", width: 285, height: 285)
                 .padding(.bottom, 259)
         }
-
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+                text = "빠직...빠드득...콰직.."
+            }       }
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                text = "뭐 나올끼니?"
+            }       }
     }
 }
 
