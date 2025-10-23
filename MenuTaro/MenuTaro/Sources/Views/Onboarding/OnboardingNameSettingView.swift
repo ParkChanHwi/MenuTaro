19b622c48b2ee1c1531770f6c1763a356d1b18b9
mac mini <macmini@macui-Macmini.local>
2025-10-23 21:15:05 +0900

    feat#16 OnboardingNameSetting

---
+++ MenuTaro/MenuTaro/Sources/Views/Onboarding/OnboardingNameSettingView.swift
@@ -0,0 +1,39 @@
+//
+//  OnboardingNameSettingView.swift
+//  MenuTaro
+//
+//  Created by mac mini on 10/23/25.
+//
+
+import SwiftUI
+
+struct OnboardingNameSettingView: View {
+    @State private var name: String = ""
+    var body: some View {
+        GeometryReader { proxy in
+            VStack (alignment : .leading, spacing : 20){
+                Text("1/3")
+                    .font(.system(.subheadline, weight: .bold))
+                    .foregroundColor(.primaryRed)
+                    .padding(.leading, proxy.size.width * 0.1)
+                Text("반가워요! 사용하실\n이름을 알려주세요.")
+                    .font(.system(.title, weight: .bold))
+                    .foregroundColor(.white)
+                    .padding(.leading, proxy.size.width * 0.1)
+                Text("*닉네임은 언제든지 바꿀 수 있어요!\n*닉네임은 한글, 영문, 숫자 (2~10자)이내로 작성해주세요!")
+                    .font(.system(.caption, weight: .regular))
+                    .foregroundColor(.white)
+                    .padding(.leading, proxy.size.width * 0.1)
+                
+                PillTextField(text: $name)
+                    .padding(.horizontal, proxy.size.width * 0.08) // 양쪽에 15% 여백 주기
+            }
+        }
+        .background(Color.black)
+        
+    }
+}
+
+#Preview {
+    OnboardingNameSettingView()
+}
