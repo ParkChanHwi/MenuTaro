dff8d91d41e8be2269f417dec06b6fdcd81de39a
zzaeun <leeje57575@gmail.com>
2025-10-29 11:33:21 +0900

    [Feat] #18 - 설정 뷰 UI 구현

---
+++ MenuTaro/MenuTaro/Sources/Views/SettingView.swift
@@ -0,0 +1,63 @@
+import SwiftUI
+
+struct SettingView: View {
+    
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    
+    // backButton 커스텀
+    var backButton: some View {
+        Button{
+            self.presentationMode.wrappedValue.dismiss()
+        } label: {
+            HStack {
+                Image(systemName: "chevron.left")
+                    .foregroundStyle(.black)
+                    .aspectRatio(contentMode: .fill)
+                    .fontWeight(.medium)
+            }
+        }
+    }
+    
+    var body: some View {
+        NavigationView {
+            VStack (alignment: .leading, spacing: 6) {
+                Text("공지사항")
+                    .padding(10)
+                    .font(.system(size: 20, weight: .bold))
+                NavigationLink(destination: NotiSettingView()) {
+                    Text("알림 설정")
+                        .padding(10)
+                }
+                Text("버그 신고")
+                    .padding(10)
+                Text("고객센터/문의하기")
+                    .padding(10)
+                Text("약관 및 개인정보 처리방침")
+                    .padding(10)
+                Text("App version: Beta")
+                    .padding(10)
+                    .font(.system(size: 18, weight: .regular))
+                    .foregroundColor(.gray)
+            }
+            .padding(.leading, -140)
+            .padding(.bottom, 410)
+            .foregroundColor(.black)
+            .font(.system(size: 18, weight: .medium))
+        }
+        .navigationBarBackButtonHidden(true)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                backButton
+            }
+            
+            ToolbarItem(placement: .principal) {
+                Text("설정")
+                    .font(.system(size: 18, weight: .semibold))
+            }
+        }
+    }
+}
+
+#Preview {
+    SettingView()
+}
