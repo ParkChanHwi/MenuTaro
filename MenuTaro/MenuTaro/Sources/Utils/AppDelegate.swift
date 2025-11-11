ab6cdf53a6cdf4479ac839013fbb7944433d5d2b
zzaeun <leeje57575@gmail.com>
2025-11-11 21:18:18 +0900

    [Feat] #35 - 로컬 푸시 알림 구현하기

--- MenuTaro/MenuTaro/Sources/Utils/AppDelegate.swift
+++ MenuTaro/MenuTaro/Sources/Utils/AppDelegate.swift
@@ -3,7 +3,12 @@ import UIKit
 class AppDelegate: NSObject, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        print("AppDelegate 성공")
+        
+        let center = UNUserNotificationCenter.current()
+        center.delegate = self
+        center.requestAuthorization(options: [.alert, .sound]) { granted, error in
+            print(granted, error)
+        }
         return true
     }
     
@@ -11,5 +16,11 @@ class AppDelegate: NSObject, UIApplicationDelegate {
         let sceneConfig = UISceneConfiguration(name: "Default Coniguration", sessionRole: connectingSceneSession.role)
         sceneConfig.delegateClass = SceneDelegate.self
         return sceneConfig
+    }
+}
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.sound, .badge, .banner])
     }
 }
