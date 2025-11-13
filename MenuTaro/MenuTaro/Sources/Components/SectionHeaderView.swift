1f1a093d0b1fa802b523212cd524acc738895932
Dolby-Jang <hjhjonjal1001@naver.com>
2025-11-13 17:45:48 +0900

    홈 캘린더 연결

--- MenuTaro/MenuTaro/Sources/Components/SectionHeaderView.swift
+++ MenuTaro/MenuTaro/Sources/Components/SectionHeaderView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct SectionHeaderView: View {
+    @EnvironmentObject private var router: Router
     let title: String
 
     var body: some View {
@@ -16,6 +17,18 @@ struct SectionHeaderView: View {
                 .foregroundColor(.white)
                 .font(.system(size: 18, weight: .medium))
             Spacer()
+            HStack(spacing: 4) {
+                Button {
+                    router.push(.calendar)
+                } label: {
+                    Text("전체보기")
+                        .font(.system(size: 12, weight: .regular, design: .default))
+                        .foregroundColor(.gray)
+                    Image(systemName: "chevron.right")
+                        .font(.system(size: 12))
+                        .foregroundColor(.gray)
+                }
+            }
         }
     }
 }
